82621468f83f9e16326d6eec73fd2fac69dea0e3
AntonBosov <x-inkvizitor@mail.ru>
2022-08-12 23:21:39 +0300

    points

--- src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -12,6 +12,7 @@ public class MyFirstProgram {
 
 		Rectangle r = new Rectangle(4,6);
 		System.out.println("S with "+r.a+" and "+r.b+"= "+r.area());
+
   }
 	public static void hello(String somebody){
 		System.out.println("Hello, "+somebody+"!");
